c015d84ec1577a1cb48912c5db7f9355f4d4481c
unknown <wrannjade@gmail.com>
2020-05-22 09:39:30 +0100

    stage 2 complete

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,16 @@
 
 console.log('it works!');
 
-document.addEventListener('click', () => {
-    const pacmanElm = document.querySelector(".entity");
-    if (pacmanElm.style.backgroundPositionX === '100%') {
-        pacmanElm.style.backgroundPositionX = '0%'
-    } else {
-        pacmanElm.style.backgroundPositionX = '100%'
-    };
+  document.addEventListener('keydown', (event) => {
+    const pacmanElm = document.querySelector(".entity")
+      if(event.key === 'ArrowRight') { 
+          const tiles = pacmanElm.style.transform += 'translate(85px, 0px)';
+        if (pacmanElm.style.backgroundPositionX === '100%') {
+            pacmanElm.style.backgroundPositionX = '0%'
+        } else {
+            pacmanElm.style.backgroundPositionX = '100%'
+        };
+      }
+    })
+
  
-  });
