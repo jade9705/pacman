24ce1b882633bc00d846b02511f324da4fb2ab5b
unknown <wrannjade@gmail.com>
2020-05-22 09:04:24 +0100

    stage one pacman

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
 'use strict';
 
 console.log('it works!');
+
+document.addEventListener('click', () => {
+    const pacmanElm = document.querySelector(".entity");
+    if (pacmanElm.style.backgroundPositionX === '100%') {
+        pacmanElm.style.backgroundPositionX = '0%'
+    } else {
+        pacmanElm.style.backgroundPositionX = '100%'
+    };
+ 
+  });
